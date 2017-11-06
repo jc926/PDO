1c727ac97dc31b0141cc2ffd1ba4c51e88c3b448
jie cai <jc926@afsconnect1.njit.edu>
2017-11-05 20:52:03 -0500

    Q1

---
+++ PDO.php
@@ -0,0 +1,25 @@
+<?php
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+
+$servername = "sql2.njit.edu";
+$username = "jc926";
+$password = "gkXLdWmKr";
+$dbname = "jc926";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+
+?>
