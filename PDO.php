1e32f6bce2437bc7b39464ef33f618fc153cba2d
jie cai <jc926@afsconnect1.njit.edu>
2017-11-05 22:37:08 -0500

    Q3.2

--- PDO.php
+++ PDO.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 
-echo "<table style='border: solid 1px black;'>";
+
 
 
 $servername = "sql2.njit.edu";
@@ -17,19 +17,33 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
-    $statement= $conn ->prepare("SELECT * from accounts where id < 6");
-    $statement->execute();
-    // $result = $statement->setFetchMode(PDO::FETCH_ASSOC); 
-    //foreach(new TableRows(new RecursiveArrayIterator($statement->fetchAll())) as $k=>$v) { 
-        //echo $v;
-    $result = $statement->fetchAll();
-    print_r($result);
+    echo "<br>";
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
+    echo "<br>";
     }
 
+    $statement= $conn ->prepare("SELECT * from accounts where id < 6");
+    $statement->execute();
+    $result = $statement->fetchAll();
+    $number_results = count($result);
+    print_r($number_results);
+    echo"<br><br>";
+    echo "<html>";
+    echo "<body>";
+    echo "<div style='overflow-x:auto;'>";
+        foreach ($result as $row) {
+              echo "<tr><td>".$row["id"]."</td><td>".$row["email"]."</td><td>".$row["fname"]."</td><td>".$row["lname"]."</td><td>".$row["phone"]."</td><td>".$row["birthday"]."</td><td>".$row["gender"]."</td><td>".$row["password"]."</td></tr>";
+           
+            }
+                      
 
+        echo "</table></div>";
+        echo "</body>";
+        echo "</html>";	
+        
+    
 
 ?>
