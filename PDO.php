33be9bab225bf053d8c78a3f10e59ff8ef1cbbd6
jie cai <jc926@afsconnect1.njit.edu>
2017-11-05 21:55:59 -0500

    Q3

--- PDO.php
+++ PDO.php
@@ -4,6 +4,9 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 
+echo "<table style='border: solid 1px black;'>";
+
+
 $servername = "sql2.njit.edu";
 $username = "jc926";
 $password = "gkXLdWmKr";
@@ -16,7 +19,11 @@ try {
     echo "Connected successfully"; 
     $statement= $conn ->prepare("SELECT * from accounts where id < 6");
     $statement->execute();
-    
+    // $result = $statement->setFetchMode(PDO::FETCH_ASSOC); 
+    //foreach(new TableRows(new RecursiveArrayIterator($statement->fetchAll())) as $k=>$v) { 
+        //echo $v;
+    $result = $statement->fetchAll();
+    print_r($result);
     }
 catch(PDOException $e)
     {
